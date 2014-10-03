ab702d55603f3baea0c751c46040b92c8083d381
Sam Spilsbury <smspillaz@gmail.com>
2014-10-03 13:33:02 +0800

    Move to polysquare's tooling-cmake-util.

--- ClangTidy.cmake
+++ ClangTidy.cmake
@@ -4,7 +4,7 @@
 # See LICENCE.md for Copyright information
 
 include (CMakeParseArguments)
-include (${CMAKE_CURRENT_LIST_DIR}/determine-header-language/DetermineHeaderLanguage.cmake)
+include (${CMAKE_CURRENT_LIST_DIR}/tooling-cmake-util/PolysquareToolingUtil.cmake)
 
 set (CLANG_TIDY_EXIT_STATUS_WRAPPER_LOCATION
      ${CMAKE_CURRENT_LIST_DIR}/util/ClangTidyExitStatusWrapper.cmake)
@@ -19,198 +19,6 @@ function (_validate_clang_tidy CONTINUE)
 endfunction ()
 
 set (CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
-
-# TODO: Deduplicate
-function (_strip_add_custom_target_sources RETURN_SOURCES TARGET)
-
-    get_target_property (_sources ${TARGET} SOURCES)
-    list (GET _sources 0 _first_source)
-    string (FIND "${_first_source}" "/" LAST_SLASH REVERSE)
-    math (EXPR LAST_SLASH "${LAST_SLASH} + 1")
-    string (SUBSTRING "${_first_source}" ${LAST_SLASH} -1 END_OF_SOURCE)
-
-    if (END_OF_SOURCE STREQUAL "${TARGET}")
-
-        list (REMOVE_AT _sources 0)
-
-    endif (END_OF_SOURCE STREQUAL "${TARGET}")
-
-    set (${RETURN_SOURCES} ${_sources} PARENT_SCOPE)
-
-endfunction ()
-
-function (_filter_out_generated_sources RESULT_VARIABLE)
-
-    set (FILTER_OUT_MUTLIVAR_OPTIONS SOURCES)
-
-    cmake_parse_arguments (FILTER_OUT
-                           ""
-                           ""
-                           "${FILTER_OUT_MUTLIVAR_OPTIONS}"
-                           ${ARGN})
-
-    set (${RESULT_VARIABLE} PARENT_SCOPE)
-    set (FILTERED_SOURCES)
-
-    foreach (SOURCE ${FILTER_OUT_SOURCES})
-
-        get_property (SOURCE_IS_GENERATED
-                      SOURCE ${SOURCE}
-                      PROPERTY GENERATED)
-
-        if (NOT SOURCE_IS_GENERATED)
-
-            list (APPEND FILTERED_SOURCES ${SOURCE})
-
-        endif (NOT SOURCE_IS_GENERATED)
-
-    endforeach ()
-
-    set (${RESULT_VARIABLE} ${FILTERED_SOURCES} PARENT_SCOPE)
-
-endfunction ()
-
-function (_psq_make_compilation_db TARGET
-                                   CUSTOM_COMPILATION_DB_DIR_RETURN)
-
-    set (MAKE_COMP_DB_OPTIONS)
-    set (MAKE_COMP_DB_SINGLEVAR_OPTIONS
-         FORCE_LANGUAGE)
-    set (MAKE_COMP_DB_MULTIVAR_OPTIONS
-         SOURCES
-         INTERNAL_INCLUDE_DIRS
-         EXTERNAL_INCLUDE_DIRS
-         DEFINES)
-
-    cmake_parse_arguments (MAKE_COMP_DB
-                           "${MAKE_COMP_DB_OPTIONS}"
-                           "${MAKE_COMP_DB_SINGLEVAR_OPTIONS}"
-                           "${MAKE_COMP_DB_MULTIVAR_OPTIONS}"
-                           ${ARGN})
-
-    set (CUSTOM_COMPILATION_DB_DIR
-         ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_compile_commands/)
-    set (COMPILATION_DB_FILE
-         ${CUSTOM_COMPILATION_DB_DIR}/compile_commands.json)
-
-    set (COMPILATION_DB_FILE_CONTENTS
-         "[")
-
-    foreach (SOURCE ${MAKE_COMP_DB_SOURCES})
-
-        get_filename_component (FULL_PATH ${SOURCE} ABSOLUTE)
-        get_filename_component (BASENAME ${SOURCE} NAME)
-
-        set (LANGUAGE ${MAKE_COMP_DB_FORCE_LANGUAGE})
-        if (NOT LANGUAGE)
-
-            # Get the language of the file.
-            polysquare_determine_language_for_source (${FULL_PATH}
-                                                      LANGUAGE
-                                                      SOURCE_WAS_HEADER
-                                                      INCLUDES
-                                                      ${ALL_INCLUDE_DIRS})
-
-        endif (NOT LANGUAGE)
-
-        set (COMPILATION_DB_FILE_CONTENTS
-             "${COMPILATION_DB_FILE_CONTENTS}\n{\n"
-             "\"directory\": \"${CMAKE_CURRENT_BINARY_DIR}\",\n"
-             "\"command\": \"")
-
-        # Compiler and language options
-        list (FIND LANGUAGE "CXX" CXX_INDEX)
-        if (NOT CXX_INDEX EQUAL -1)
-
-             set (COMPILATION_DB_FILE_CONTENTS
-                  "${COMPILATION_DB_FILE_CONTENTS}${CMAKE_CXX_COMPILER} -x c++")
-
-        else (NOT CXX_INDEX EQUAL -1)
-
-            set (COMPILATION_DB_FILE_CONTENTS
-                 "${COMPILATION_DB_FILE_CONTENTS}${CMAKE_C_COMPILER}")
-
-        endif (NOT CXX_INDEX EQUAL -1)
-
-        # Fake output file etc.
-        set (COMPILATION_DB_FILE_CONTENTS
-             "${COMPILATION_DB_FILE_CONTENTS}"
-             " -o CMakeFiles/${TARGET}.dir/${BASENAME}.o"
-             " -c ${FULL_PATH}")
-
-        # All includes
-        foreach (INTERNAL_INCLUDE ${MAKE_COMP_DB_INTERNAL_INCLUDE_DIRS})
-
-            set (COMPILATION_DB_FILE_CONTENTS
-                 "${COMPILATION_DB_FILE_CONTENTS} -I${INTERNAL_INCLUDE} ")
-
-        endforeach ()
-
-        foreach (EXTERNAL_INCLUDE ${MAKE_COMP_DB_EXTERNAL_INCLUDE_DIRS})
-
-            set (COMPILATION_DB_FILE_CONTENTS
-                 "${COMPILATION_DB_FILE_CONTENTS}"
-                 "-isystem${EXTERNAL_INCLUDE} ")
-
-        endforeach ()
-
-        # All defines
-        foreach (DEFINE ${MAKE_COMP_DB_DEFINES})
-
-            set (COMPILATION_DB_FILE_CONTENTS
-                 "${COMPILATION_DB_FILE_CONTENTS} -D${DEFINE}")
-
-        endforeach ()
-
-        # CXXFLAGS / CFLAGS
-
-        if (NOT CXX_INDEX EQUAL -1)
-
-            # Add CMAKE_CXX_FLAGS
-            set (COMPILATION_DB_FILE_CONTENTS
-                 "${COMPILATION_DB_FILE_CONTENTS} ${CMAKE_CXX_FLAGS}")
-
-        else (NOT CXX_INDEX EQUAL -1)
-
-            set (COMPILATION_DB_FILE_CONTENTS
-                 "${COMPILATION_DB_FILE_CONTENTS} ${CMAKE_C_FLAGS}")
-
-        endif (NOT CXX_INDEX EQUAL -1)
-
-        set (COMPILATION_DB_FILE_CONTENTS
-             "${COMPILATION_DB_FILE_CONTENTS}\",\n"
-             "\"file\": \"${FULL_PATH}\"\n"
-             "},")
-
-    endforeach ()
-
-    # Get rid of all the semicolons
-    string (REPLACE ";" ""
-            COMPILATION_DB_FILE_CONTENTS
-            "${COMPILATION_DB_FILE_CONTENTS}")
-
-    # Take away the last comma
-    string (LENGTH
-            "${COMPILATION_DB_FILE_CONTENTS}"
-            COMPILATION_DB_FILE_LENGTH)
-    math (EXPR TRIMMED_COMPILATION_DB_FILE_LENGTH
-          "${COMPILATION_DB_FILE_LENGTH} - 1")
-    string (SUBSTRING "${COMPILATION_DB_FILE_CONTENTS}"
-            0 ${TRIMMED_COMPILATION_DB_FILE_LENGTH}
-            COMPILATION_DB_FILE_CONTENTS)
-
-    # Final "]"
-    set (COMPILATION_DB_FILE_CONTENTS
-         "${COMPILATION_DB_FILE_CONTENTS}\n]\n")
-
-    # Write out
-    file (WRITE ${COMPILATION_DB_FILE}
-          ${COMPILATION_DB_FILE_CONTENTS})
-
-    set (${CUSTOM_COMPILATION_DB_DIR_RETURN}
-         ${CUSTOM_COMPILATION_DB_DIR} PARENT_SCOPE)
-
-endfunction (_psq_make_compilation_db)
 
 function (clang_tidy_check_target_sources TARGET)
 
@@ -233,41 +41,32 @@ function (clang_tidy_check_target_sources TARGET)
                            "${CHECK_SOURCES_MULTIVAR_OPTIONS}"
                            ${ARGN})
 
-    _strip_add_custom_target_sources (FILES_TO_CHECK ${TARGET})
-
-    if (NOT CHECK_SOURCES_CHECK_GENERATED)
-        _filter_out_generated_sources (FILES_TO_CHECK
+    psq_strip_add_custom_target_sources (FILES_TO_CHECK ${TARGET})
+    psq_handle_check_generated_option (CHECK_SOURCES FILES_TO_CHECK
                                        SOURCES ${FILES_TO_CHECK})
-    endif (NOT CHECK_SOURCES_CHECK_GENERATED)
 
-    set (WARN_ONLY OFF)
-    if (CHECK_SOURCES_WARN_ONLY)
-        set (WARN_ONLY ON)
-    endif (CHECK_SOURCES_WARN_ONLY)
+    string (REPLACE ";" "," ENABLE_CHECKS_LIST "${CHECK_SOURCES_ENABLE_CHECKS}")
+    string (REPLACE ";" "," DISABLE_CHECKS_LIST "${CHECK_SOURCES_DISABLE_CHECKS}")
+
+    set (CLANG_TIDY_OPTIONS
+         -DVERBOSE=${CMAKE_VERBOSE_MAKEFILE}
+         -DCLANG_TIDY_EXECUTABLE=${CLANG_TIDY_EXECUTABLE}
+         -DENABLE_CHECKS="${ENABLE_CHECKS_LIST}"
+         -DDISABLE_CHECKS="${DISABLE_CHECKS_LIST}")
+
+    psq_add_switch (CLANG_TIDY_OPTIONS CHECK_SOURCES_WARN_ONLY
+                    ON -DWARN_ONLY=ON
+                    OFF -DWARN_ONLY=OFF)
 
     # Figure out if this target is linkable. If it is a UTILITY
     # target then we need to run the checks at the PRE_BUILD stage.
-    set (WHEN PRE_LINK)
-
-    get_property (TARGET_TYPE
-                  TARGET ${TARGET}
-                  PROPERTY TYPE)
+    psq_get_target_command_attach_point (${TARGET} WHEN)
 
     # Scan each source file to determine its language. We might need
     # it later when generating a compilation database.
     set (ALL_INCLUDE_DIRS
          ${CHECK_SOURCES_INTERNAL_INCLUDE_DIRS}
          ${CHECK_SOURCES_EXTERNAL_INCLUDE_DIRS})
-
-    foreach (SOURCE ${FILES_TO_CHECK})
-
-        polysquare_scan_source_for_headers (SOURCE ${SOURCE}
-                                            INCLUDES
-                                            ${ALL_INCLUDE_DIRS}
-                                            CPP_IDENTIFIERS
-                                            ${CHECK_SOURCES_CPP_IDENTIFIERS})
-
-    endforeach ()
 
     if (CHECK_SOURCES_FORCE_LANGUAGE)
 
@@ -276,52 +75,53 @@ function (clang_tidy_check_target_sources TARGET)
 
     endif (CHECK_SOURCES_FORCE_LANGUAGE)
 
+    # Scan source languages and sort them out now. We will pass
+    # each of these lists to psq_make_compilation_db for later
+    # use.
+    psq_forward_options (CHECK_SOURCES SORT_TO_LANGS_FORWARD_OPTIONS
+                         SINGLEVAR_ARGS FORCE_LANGUAGE
+                         MULTIVAR_ARGS CPP_IDENTIFIERS)
+    psq_sort_sources_to_languages (C_SOURCES CXX_SOURCES HEADERS
+                                   SOURCES ${FILES_TO_CHECK}
+                                   ${SORT_TO_LANGS_FORWARD_OPTIONS}
+                                   INCLUDES ${ALL_INCLUDE_DIRS})
+
     # Special rules apply for UTILITY type targets. We need to run
     # the tool PRE_BUILD as opposed to PRE_LINK.
-    if (TARGET_TYPE STREQUAL "UTILITY")
+    if (WHEN STREQUAL "PRE_BUILD")
 
-        set (WHEN PRE_BUILD)
         set (CUSTOM_COMPILATION_DB_SOURCES ${FILES_TO_CHECK})
 
-    else (TARGET_TYPE STREQUAL "UTILITY")
+    else (WHEN STREQUAL "PRE_BUILD")
 
-        # Separate out headers and real sources
-        foreach (SOURCE ${FILES_TO_CHECK})
+        # Add headers to compilation DB for linkable
+        set (CUSTOM_COMPILATION_DB_SOURCES ${HEADERS})
 
-            polysquare_determine_language_for_source (${SOURCE}
-                                                      LANGUAGE
-                                                      SOURCE_WAS_HEADER
-                                                      INCLUDES
-                                                      ${ALL_INCLUDE_DIRS})
+    endif (WHEN STREQUAL "PRE_BUILD")
 
-            if (SOURCE_WAS_HEADER)
-
-                list (APPEND CUSTOM_COMPILATION_DB_SOURCES ${SOURCE})
-
-            endif (SOURCE_WAS_HEADER)
-
-        endforeach ()
-
-    endif (TARGET_TYPE STREQUAL "UTILITY")
+    # Keep C_SOURCES and CXX_SOURCES intersecting with the sources
+    # we wish to make a compilation DB for.
+    psq_get_list_intersection (C_COMP_DB_SOURCES
+                                SOURCE ${C_SOURCES}
+                                INTERSECTION ${CUSTOM_COMPILATION_DB_SOURCES})
+    psq_get_list_intersection (CXX_COMP_DB_SOURCES
+                                SOURCE ${CXX_SOURCES}
+                                INTERSECTION ${CUSTOM_COMPILATION_DB_SOURCES})
 
     # For mixed source-header targets or UTILITY targets, we need
     # to generate a fake compilation database as CMake won't do it
     # for us in this instance.
-    _psq_make_compilation_db (${TARGET}
-                              CUSTOM_COMPILATION_DB_DIR
-                              ${FORCE_LANGUAGE_OPTION}
-                              SOURCES
-                              ${CUSTOM_COMPILATION_DB_SOURCES}
-                              INTERNAL_INCLUDE_DIRS
-                              ${CHECK_SOURCES_INTERNAL_INCLUDE_DIRS}
-                              EXTERNAL_INCLUDE_DIRS
-                              ${CHECK_SOURCES_EXTERNAL_INCLUDE_DIRS}
-                              DEFINES
-                              ${CHECK_SOURCES_DEFINES})
-
-    string (REPLACE ";" "," ENABLE_CHECKS_LIST "${CHECK_SOURCES_ENABLE_CHECKS}")
-    string (REPLACE ";" "," DISABLE_CHECKS_LIST "${CHECK_SOURCES_DISABLE_CHECKS}")
-
+    psq_make_compilation_db (${TARGET}
+                             CUSTOM_COMPILATION_DB_DIR
+                             ${FORCE_LANGUAGE_OPTION}
+                             C_SOURCES ${C_COMP_DB_SOURCES}
+                             CXX_SOURCES ${CXX_COMP_DB_SOURCES}
+                             INTERNAL_INCLUDE_DIRS
+                             ${CHECK_SOURCES_INTERNAL_INCLUDE_DIRS}
+                             EXTERNAL_INCLUDE_DIRS
+                             ${CHECK_SOURCES_EXTERNAL_INCLUDE_DIRS}
+                             DEFINES
+                             ${CHECK_SOURCES_DEFINES})
 
     foreach (SOURCE ${FILES_TO_CHECK})
 
@@ -345,13 +145,9 @@ function (clang_tidy_check_target_sources TARGET)
                             ${WHEN}
                             COMMAND
                             ${CMAKE_COMMAND}
-                            -DVERBOSE=${CMAKE_VERBOSE_MAKEFILE}
-                            -DWARN_ONLY=${WARN_ONLY}
-                            -DCLANG_TIDY_EXECUTABLE=${CLANG_TIDY_EXECUTABLE}
-                            -DENABLE_CHECKS="${ENABLE_CHECKS_LIST}"
-                            -DDISABLE_CHECKS="${DISABLE_CHECKS_LIST}"
                             -DSOURCE=${SOURCE}
                             -DCUSTOM_COMPILATION_DB_DIR=${SOURCE_COMP_DB}
+                            ${CLANG_TIDY_OPTIONS}
                             -P
                             ${CLANG_TIDY_EXIT_STATUS_WRAPPER_LOCATION})
     endforeach ()
--- test/ClangTidySpecialCompilationDBUseCCompilerForCHeaderVerify.cmake
+++ test/ClangTidySpecialCompilationDBUseCCompilerForCHeaderVerify.cmake
@@ -8,6 +8,5 @@ include (${CLANG_TIDY_CMAKE_TESTS_DIRECTORY}/CMakeUnit.cmake)
 
 set (COMPILE_COMMANDS
      ${CMAKE_CURRENT_BINARY_DIR}/target_compile_commands/compile_commands.json)
-message ("C Compiler ${CMAKE_CURRENT_BINARY_DIR}")
 assert_file_has_line_matching (${COMPILE_COMMANDS}
                                "^.*${CMAKE_C_COMPILER}.*Header.h.*$")
