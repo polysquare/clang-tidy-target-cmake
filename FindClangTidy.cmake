522f17c953b94e3611429fb8070a4de11279b7d6
Sam Spilsbury <smspillaz@gmail.com>
2014-10-26 18:03:25 +0800

    Move to newer tooling-find-package-cmake-util

--- FindClangTidy.cmake
+++ FindClangTidy.cmake
@@ -16,7 +16,11 @@
 #
 # See LICENCE.md for Copyright info
 
-include (${CMAKE_CURRENT_LIST_DIR}/tooling-find-package-cmake-util/ToolingFindPackageUtil.cmake)
+set (CMAKE_MODULE_PATH
+     ${CMAKE_MODULE_PATH}
+     ${CMAKE_CURRENT_LIST_DIR}/tooling-find-package-cmake-util)
+
+include (ToolingFindPackageUtil)
 
 function (_find_clang_tidy)
 
@@ -48,38 +52,23 @@ function (_find_clang_tidy)
                                        VERSION_HEADER
                                        "${CLANG_TIDY_VERSION_HEADER}"
                                        VERSION_END_TOKEN "\n")
-        psq_check_and_report_tool_version (ClangTidy
-                                           ${CLANG_TIDY_VERSION}
-                                           FOUND_APPROPRIATE_VERSION)
-
-        # If we found all the paths set ClangTidy_FOUND and other related
-        # variables
-        if (FOUND_APPROPRIATE_VERSION)
-
-            set (ClangTidy_FOUND TRUE)
-            set (CLANG_TIDY_FOUND TRUE PARENT_SCOPE)
-            set (CLANG_TIDY_EXECUTABLE ${CLANG_TIDY_EXECUTABLE} PARENT_SCOPE)
-            set (CLANG_TIDY_VERSION ${CLANG_TIDY_VERSION} PARENT_SCOPE)
-
-            psq_print_if_not_quiet (ClangTidy "ClangTidy version"
-                                              "${CLANG_TIDY_VERSION} found at"
-                                              "${CLANG_TIDY_EXECUTABLE}")
-
-        else (FOUND_APPROPRIATE_VERSION)
-
-            set (ClangTidy_FOUND FALSE)
-
-        endif (FOUND_APPROPRIATE_VERSION)
 
     endif (CLANG_TIDY_EXECUTABLE)
 
+    psq_check_and_report_tool_version (ClangTidy
+                                       "${CLANG_TIDY_VERSION}"
+                                       REQUIRED_VARS
+                                       CLANG_TIDY_EXECUTABLE
+                                       CLANG_TIDY_VERSION)
+
+    psq_print_if_not_quiet (ClangTidy
+                            MSG "ClangTidy version"
+                                "${CLANG_TIDY_VERSION} found at"
+                                "${CLANG_TIDY_EXECUTABLE}"
+                            DEPENDS CLANG_TIDY_VERSION
+                                    CLANG_TIDY_EXECUTABLE)
+
     set (ClangTidy_FOUND ${ClangTidy_FOUND} PARENT_SCOPE)
-
-    if (NOT ClangTidy_FOUND)
-
-        psq_report_tool_not_found (ClangTidy "ClangTidy was not found")
-
-    endif (NOT ClangTidy_FOUND)
 
 endfunction (_find_clang_tidy)
 
