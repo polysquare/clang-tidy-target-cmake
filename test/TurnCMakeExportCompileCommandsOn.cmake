0db67e64fe3e822ba095c0d39585f7722418c1c2
Sam Spilsbury <smspillaz@gmail.com>
2015-10-21 12:26:36 +0800

    Move to newer cmake-unit and ci scripts

--- test/TurnCMakeExportCompileCommandsOn.cmake
+++
@@ -1,9 +0,0 @@
-# /tests/TurnCMakeExportCompileCommandsOn.cmake
-# Check that including ClangTidy.cmake turns CMAKE_EXPORT_COMPILE_COMMANDS ON
-#
-# See LICENCE.md for Copyright information
-
-include (CMakeUnit)
-include (ClangTidy)
-
-assert_true (${CMAKE_EXPORT_COMPILE_COMMANDS})
