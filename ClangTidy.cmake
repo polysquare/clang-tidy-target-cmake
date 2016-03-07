e3304d78e5d4b30dfa72112ca98305d1050ec5b0
Sam Spilsbury <smspillaz@gmail.com>
2016-03-07 14:14:54 +0800

    conan: Add support for deploying to conan
    Remove support for deploying to biicode

--- ClangTidy.cmake
+++ ClangTidy.cmake
@@ -5,7 +5,7 @@
 # See /LICENCE.md for Copyright information
 
 include (CMakeParseArguments)
-include ("smspillaz/tooling-cmake-util/PolysquareToolingUtil")
+include ("cmake/tooling-cmake-util/PolysquareToolingUtil")
 
 set (CLANG_TIDY_EXIT_STATUS_WRAPPER_LOCATION
      "${CMAKE_CURRENT_LIST_DIR}/util/ClangTidyExitStatusWrapper.cmake")
